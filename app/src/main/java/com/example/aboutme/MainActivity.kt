96cf45910e0c6e31ac7ea8e04f0bd5c9f620f022
cauehenrique <caue313@gmail.com>
2022-04-11 08:47:40 -0300

    reverse to 1.0

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,35 +1,11 @@
 package com.example.aboutme
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById<Button>(R.id.doneButton).setOnClickListener {
-            addName(it);
-        }
-    }
-
-    private fun addName(view: View) {
-        val nameTextEdit = findViewById<EditText>(R.id.nameEditText);
-        val nameTextView = findViewById<TextView>(R.id.nameText);
-
-        nameTextView.text = nameTextEdit.text;
-        nameTextView.visibility = View.VISIBLE;
-
-        nameTextEdit.visibility = View.GONE;
-        view.visibility = View.GONE;
-
-        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager;
-        inputManager.hideSoftInputFromWindow(view.windowToken, 0);
     }
 }
